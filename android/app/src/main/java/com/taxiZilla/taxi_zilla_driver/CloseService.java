186d12f021ccf18b894acae44d028da9523158ce
Delyan Boychev <delyan.boychev05@gmail.com>
2021-04-03 11:57:24 +0300

    Version 1.0.9 Fixed not suspending, not sleeping

--- android/app/src/main/java/com/taxiZilla/taxi_zilla_driver/CloseService.java
+++ android/app/src/main/java/com/taxiZilla/taxi_zilla_driver/CloseService.java
@@ -27,7 +27,7 @@ public class CloseService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_NOT_STICKY;
+        return START_STICKY;
     }
 
     @Override
--- android/app/src/main/java/com/taxiZilla/taxi_zilla_driver/MainActivity.java
+++ android/app/src/main/java/com/taxiZilla/taxi_zilla_driver/MainActivity.java
@@ -3,6 +3,7 @@ package com.taxiZilla.taxi_zilla_driver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
@@ -12,6 +13,7 @@ public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstnceState) {
         super.onCreate(savedInstnceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         Intent intent = new Intent(getBaseContext(), CloseService.class);
         startService(intent);
         MethodChannel chan = new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL);
